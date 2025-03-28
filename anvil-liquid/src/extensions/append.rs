f1a77735ff32422544f7c893b63525765492ea34
sjcobb <sjc@incyan.com>
2025-03-28 00:38:45 +0000

    init liquid

---
+++ anvil-liquid/src/extensions/append.rs
@@ -0,0 +1,87 @@
+use anvil::{append::Append, Forge};
+
+use crate::{Aqua, Water};
+
+pub trait TeraAppendExt<'a, T: Water>: Forge {
+    fn liquid(template: &'a T) -> Self;
+}
+
+impl<'a, T: Water> TeraAppendExt<'a, T> for Append<Aqua<'a, T>> {
+    fn liquid(template: &'a T) -> Self {
+        Self::new(Aqua(template))
+    }
+}
+
+#[inline(always)]
+pub fn append<T: Water>(template: &T) -> Append<Aqua<'_, T>> {
+    Append::liquid(template)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::make_liquid_template;
+
+    use super::*;
+    use liquid::ParserBuilder;
+    use serde::Serialize;
+    use std::{fs::File, io::Write, sync::LazyLock};
+    use tempfile::tempdir;
+
+    static PARSER: LazyLock<liquid::Parser> =
+        LazyLock::new(|| ParserBuilder::with_stdlib().build().unwrap());
+
+    // NOTE: This template needs the dummy braces to be recognized by Tera because a completely
+    // empty struct us not parseable json.
+    #[derive(Serialize)]
+    struct TestTemplate {}
+
+    impl Water for TestTemplate {
+        fn liquid(&self, writer: &mut dyn std::io::Write) -> Result<(), liquid::Error> {
+            let object = liquid::to_object(self)?;
+            let template = PARSER.parse("Appended content.")?;
+            template.render_to(writer, &object)
+        }
+    }
+
+    #[test]
+    fn it_fails_if_file_does_not_exist() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("my-temporary-note.txt");
+        let result = append(&TestTemplate {}).forge(&file_path);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn it_appends_to_file() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("my-temporary-note.txt");
+        let mut file = File::create(&file_path).unwrap();
+        writeln!(file, "Initial content.").unwrap();
+        let result = append(&TestTemplate {}).forge(&file_path);
+        assert!(result.is_ok());
+        let content = std::fs::read_to_string(&file_path).unwrap();
+        assert_eq!(content, "Initial content.\nAppended content.")
+    }
+
+    #[derive(Serialize)]
+    struct TestFile {
+        name: String,
+    }
+
+    make_liquid_template!(TestFile, "templates/test.txt", PARSER);
+
+    #[test]
+    fn it_can_render_from_file() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("my-temporary-note.txt");
+        let mut file = File::create(&file_path).unwrap();
+        writeln!(file, "Initial content.").unwrap();
+        let result = append(&TestFile {
+            name: "World".to_string(),
+        })
+        .forge(&file_path);
+        assert!(result.is_ok());
+        let content = std::fs::read_to_string(&file_path).unwrap();
+        assert_eq!(content, "Initial content.\nHello, World!\n");
+    }
+}
--- anvil-liquid/src/lib.rs
+++ anvil-liquid/src/lib.rs
@@ -1,14 +1,81 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::io::Write;
+
+use anvil::Anvil;
+use serde::Serialize;
+
+pub mod extensions;
+
+pub trait Water: Serialize {
+    fn liquid(&self, writer: &mut dyn Write) -> Result<(), liquid::Error>;
+}
+
+pub struct Aqua<'a, T: Water>(&'a T);
+
+impl<T: Water> Anvil for Aqua<'_, T> {
+    type Error = liquid::Error;
+
+    fn anvil(&self, writer: &mut (impl std::io::Write + Sized)) -> Result<(), Self::Error> {
+        self.0.liquid(writer)
+    }
+}
+
+pub mod prelude {
+    pub use crate::Water;
+    // Export any extensions here similar to Tera implementation
+}
+
+#[macro_export]
+macro_rules! make_liquid_template {
+    ($struct:ident, $template:expr, $parser:expr) => {
+        impl Water for $struct {
+            fn liquid(&self, writer: &mut dyn std::io::Write) -> Result<(), liquid::Error> {
+                let object = liquid::to_object(self)?;
+                let template = $parser.parse_file($template)?;
+                template.render_to(writer, &object)
+            }
+        }
+    };
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod test {
+    use crate::{Aqua, Water};
+    use anvil::Anvil;
+    use liquid::ParserBuilder;
+    use serde::Serialize;
+    use std::sync::LazyLock;
+
+    static PARSER: LazyLock<liquid::Parser> =
+        LazyLock::new(|| ParserBuilder::with_stdlib().build().unwrap());
+
+    #[derive(Serialize)]
+    struct TestTemplate {}
+
+    make_liquid_template!(TestTemplate, "test", PARSER);
+
+    #[derive(Serialize)]
+    struct TestWater {
+        name: String,
+    }
+
+    impl Water for TestWater {
+        fn liquid(&self, writer: &mut dyn std::io::Write) -> Result<(), liquid::Error> {
+            let object = liquid::to_object(self)?;
+            let template = PARSER.parse("Hello, {{ name }}!")?;
+            template.render_to(writer, &object)
+        }
+    }
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn it_can_render_template() {
+        let template = TestWater {
+            name: "World".to_string(),
+        };
+
+        let mut buf = Vec::new();
+        let aqua = Aqua(&template);
+        aqua.anvil(&mut buf).unwrap();
+        let result = String::from_utf8(buf).unwrap();
+        assert_eq!(result, "Hello, World!");
     }
 }
