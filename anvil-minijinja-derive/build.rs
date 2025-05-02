ee374e4ca8d87b458d9c527481027d652250be09
sjcobb <sjc@incyan.com>
2025-05-02 10:39:37 +0100

    only compile embedded in build

--- anvil-minijinja-derive/build.rs
+++ anvil-minijinja-derive/build.rs
@@ -1,4 +1,6 @@
 fn main() {
-    println!("cargo:rerun-if-changed=tests/templates");
-    minijinja_embed::embed_templates!("tests/templates/");
+    if cfg!(test) {
+        println!("cargo:rerun-if-changed=tests/templates");
+        minijinja_embed::embed_templates!("tests/templates/");
+    }
 }
