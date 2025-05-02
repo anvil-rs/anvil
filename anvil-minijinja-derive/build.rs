433cd8441aefe10dc19e88b96097062aa3aa3f11
sjcobb <sjc@incyan.com>
2025-05-02 10:42:41 +0100

    remove conditional content

--- anvil-minijinja-derive/build.rs
+++ anvil-minijinja-derive/build.rs
@@ -1,6 +1,4 @@
 fn main() {
-    if cfg!(test) {
-        println!("cargo:rerun-if-changed=tests/templates");
-        minijinja_embed::embed_templates!("tests/templates/");
-    }
+    println!("cargo:rerun-if-changed=tests/templates");
+    minijinja_embed::embed_templates!("tests/templates/");
 }
