5eb47ab855f5dee70e331860cdaeb5d645a2c01b
sjcobb <sjc@incyan.com>
2025-05-07 09:58:06 +0100

    nicer workspace handling

--- anvil-liquid-derive/src/lib.rs
+++ anvil-liquid-derive/src/lib.rs
@@ -1,8 +1,7 @@
 use proc_macro::TokenStream;
 use quote::quote;
+use std::path::PathBuf;
 use syn::{parse_macro_input, DeriveInput, Error, Expr, ExprLit, Lit, Meta};
-
-// Import all required
 
 /// Derives the `anvil_liquid::Water` trait for a struct.
 ///
@@ -70,16 +69,23 @@ pub fn derive_template(input: TokenStream) -> TokenStream {
     let template_name = format!("_LIQUID_TEMPLATE_{}", name).to_uppercase();
     let template_ident = syn::Ident::new(&template_name, name.span());
 
+    let template_path = [env!("CARGO_MANIFEST_DIR"), &template_path]
+        .iter()
+        .collect::<PathBuf>()
+        .into_os_string()
+        .into_string()
+        .unwrap();
+
     // Generate the static template initialization
     let template_init = if let Some(parser) = parser {
         quote! {
             static #template_ident: ::std::sync::LazyLock<::liquid::Template> =
-                ::std::sync::LazyLock::new(|| #parser.parse(include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/", #template_path))).unwrap());
+                ::std::sync::LazyLock::new(|| #parser.parse(include_str!(#template_path)).unwrap());
         }
     } else {
         quote! {
             static #template_ident: ::std::sync::LazyLock<::liquid::Template> =
-                ::std::sync::LazyLock::new(|| ::liquid::ParserBuilder::with_stdlib().build().unwrap().parse(include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/", #template_path))).unwrap());
+                ::std::sync::LazyLock::new(|| ::liquid::ParserBuilder::with_stdlib().build().unwrap().parse(include_str!(#template_path)).unwrap());
         }
     };
 
